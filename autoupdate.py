11e5e05fc40451f325ea7ae5a9b9cc16c86b808d
cavernicol4 <pperezc@protonmail.com>
2022-01-15 18:46:44 -0300

    prueba

---
+++ autoupdate.py
@@ -0,0 +1,17 @@
+import base64
+from github import Github
+from github import InputGitTreeElement
+import subprocess
+
+user = "cavernicol4"
+token = "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"
+commit_message = "prueba"
+g = Github(user,token)
+repo = g.get_user().get_repo('phishing-feeds-') # repo name
+file_list = [
+    'phishing-feeds-/prueba2.txt',
+    'phishing-feeds-/diarios/prueba3.txt'
+            ]
+subprocess.call(['git', 'add', '-A'])
+subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
+subprocess.call(['git', 'push', 'https://{}@github.com/user-name/repo.git'.format(token)])
