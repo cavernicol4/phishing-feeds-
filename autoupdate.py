f2a0fcdc3cc559c59c82422c891c1410b9c5ff7c
cavernicol4 <pperezc@protonmail.com>
2022-01-15 21:21:25 -0300

    prueba

--- autoupdate.py
+++ autoupdate.py
@@ -4,7 +4,7 @@ from github import InputGitTreeElement
 import subprocess
 
 user = "cavernicol4"
-token = "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"
+token = "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"
 commit_message = "prueba"
 g = Github(user,token)
 repo = g.get_user().get_repo('phishing-feeds-') # repo name
@@ -14,5 +14,5 @@ file_list = [
             ]
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
-subprocess.call(['git', 'push', 'https://{}@github.com/cavernicol4/phishing-feeds-'.format(token)])
-#subprocess.call(['git', 'push', '--set-upstream', 'https://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@github.com/cavernicola/phishing-feeds- master'])
+#subprocess.call(['git', 'push', 'https://{}@github.com/cavernicol4/phishing-feeds-'.format(token)])
+subprocess.call(['git', 'push', '--set-upstream', 'https://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@github.com/cavernicola/phishing-feeds- master'])
